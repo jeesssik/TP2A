2b99cf720e07c75327b18c68f7ce8a27be5d7331
jeesssik <jeesssik@gmail.com>
2020-04-03 15:35:14 -0300

    codigo atomizado

--- desafio1/JesicaRodriguez.js
+++ desafio1/JesicaRodriguez.js
@@ -4,21 +4,9 @@ let data = require('./data');
 
 const items = data.split('<');
 /* busca flex box video y arma array nuevo*/
-//console.log("\nMuestro solo Flexbox video\n");
-var filteredArray = items.filter((str)=>{return str.includes('Flexbox Video'); });
 
-//console.log(filteredArray);
+var filteredArray = items.filter((str)=>{return str.includes('Flexbox Video'); }).map((str)=>{return(str.slice(str.indexOf('"')+1,str.lastIndexOf('"')).split(':'));});
 
-console.log("\nsolo de los tiempos de Flexbox video\n");
-var mapeado= filteredArray.map((str)=>{
-	return(str.slice(str.indexOf('"')+1,str.lastIndexOf('"')).split(':'));
-
-});
+console.log(filteredArray);
 
 
-
-
-
-
-
-console.log(mapeado)
