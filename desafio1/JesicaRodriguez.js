5eb25fd77ca486e570aa964b917c98c0e6fda91e
jeesssik <jeesssik@gmail.com>
2020-04-03 15:17:48 -0300

    reeducccion de codigo

--- desafio1/JesicaRodriguez.js
+++ desafio1/JesicaRodriguez.js
@@ -3,24 +3,22 @@ let data = require('./data');
 
 
 const items = data.split('<');
-
-
 /* busca flex box video y arma array nuevo*/
-console.log("Muestro solo Flexbox video")
-var textToSearch = "Flexbox Video";
-var filteredArray = items.filter((str)=>{
-  return str.toLowerCase().indexOf(textToSearch.toLowerCase()) >= 0; 
+//console.log("\nMuestro solo Flexbox video\n");
+var filteredArray = items.filter((str)=>{return str.includes('Flexbox Video'); });
+
+//console.log(filteredArray);
+
+console.log("\nsolo de los tiempos de Flexbox video\n");
+var mapeado= filteredArray.map((str)=>{
+	return(str.slice(str.indexOf('"')+1,str.lastIndexOf('"')).split(':'));
+
 });
 
 
-console.log(filteredArray)
-
-console.log("-------------------------")
-/* arma array solo de los tiempos*/
-var filter= filteredArray.map((str)=>{
-	return(str.slice(str.indexOf('"')+1,str.lastIndexOf('"')));
-});
 
 
-console.log(filter)
-console.log(filter[1])
+
+
+
+console.log(mapeado)
